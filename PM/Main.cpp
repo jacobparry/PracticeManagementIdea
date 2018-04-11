3d088656cc1a867bfb616bedc8aff9fd748c6433
Jacob Parry <griever815@gmail.com>
2018-04-11 17:37:59 -0600

    First Commit

---
+++ PM/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int count = 0;
+
+	while (count <= 1000)
+	{
+		//cout << count << endl;
+		count++;
+	}
+	
+
+	cout << "done" << endl;
+	system("pause");
+
+	return 0;
+}
+
+
+
+
+
+/*
+crystal PM works fine, but it doesn't do everything. Needs more complex things than what they offer. Everything they use is an shared excel sheet for things like wait list.
+crystal PM does scheduling and billing
+
+want a couple of tabs for like wait lists, tab for a schedule that can
+tab for tasks, kinda like wunderlist, assign to different employees according shift or chose employee.
+tab for daily reciepts.
+tab for statistics, ie number of new patients, appointments, etc over the month.
+
+everything needs to be on a local server.
+*/
